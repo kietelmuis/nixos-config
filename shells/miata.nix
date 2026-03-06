8a53379f73f8f94e1e4b856e82a50cc0c7f0d92a
kietelmuis <omar.hopman9@gmail.com>
2026-03-06 01:55:14 +0100

    fix: denv error

--- shells/miata.nix
+++ shells/miata.nix
@@ -1,53 +1,23 @@
-{
-  description = "Miata Dash environment";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.protobuf
-          pkgs.cmake
-          pkgs.boost
-          pkgs.openssl
-          pkgs.libusb1
-          pkgs.rtaudio
-          pkgs.qt5.qtbase
-          pkgs.qt5.qtmultimedia
-          pkgs.qt5.qtconnectivity
-          pkgs.qt5.qtwayland
-          pkgs.gst_all_1.gstreamer
-          pkgs.gst_all_1.gst-plugins-base
-          pkgs.gst_all_1.gst-plugins-good
-        ];
-        shellHook = ''
-          export QT_QPA_PLATFORM=wayland
-          export GST_PLUGIN_SYSTEM_PATH_1_0="${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0"
-          exec zsh
-        '';
-      };
-    };
+{ pkgs }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.protobuf
+    pkgs.cmake
+    pkgs.boost
+    pkgs.openssl
+    pkgs.libusb1
+    pkgs.rtaudio
+    pkgs.qt5.qtbase
+    pkgs.qt5.qtmultimedia
+    pkgs.qt5.qtconnectivity
+    pkgs.qt5.qtwayland
+    pkgs.gst_all_1.gstreamer
+    pkgs.gst_all_1.gst-plugins-base
+    pkgs.gst_all_1.gst-plugins-good
+  ];
+  shellHook = ''
+    export QT_QPA_PLATFORM=wayland
+    export GST_PLUGIN_SYSTEM_PATH_1_0="${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0"
+    exec zsh
+  '';
 }
--- shells/wsteam.nix
+++ shells/wsteam.nix
@@ -1,60 +1,30 @@
-{
-  description = "wsteam";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.xorg.libX11
-          pkgs.xorg.libICE
-          pkgs.xorg.libSM
-          pkgs.xorg.libXext
-          pkgs.xorg.libXrender
-          pkgs.xorg.libXcursor
-          pkgs.xorg.libXfixes
-          pkgs.xorg.libXi
-          pkgs.xorg.libXrandr
-        ];
-        shellHook = ''
-          export LD_LIBRARY_PATH="${
-            pkgs.lib.makeLibraryPath [
-              pkgs.xorg.libX11
-              pkgs.xorg.libICE
-              pkgs.xorg.libSM
-              pkgs.xorg.libXext
-              pkgs.xorg.libXrender
-              pkgs.xorg.libXcursor
-              pkgs.xorg.libXfixes
-              pkgs.xorg.libXi
-              pkgs.xorg.libXrandr
-            ]
-          }:$LD_LIBRARY_PATH"
-          exec zsh
-        '';
-      };
-    };
+{ pkgs }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.libX11
+    pkgs.libICE
+    pkgs.libSM
+    pkgs.libXext
+    pkgs.libXrender
+    pkgs.libXcursor
+    pkgs.libXfixes
+    pkgs.libXi
+    pkgs.libXrandr
+  ];
+  shellHook = ''
+    export LD_LIBRARY_PATH="${
+      pkgs.lib.makeLibraryPath [
+        pkgs.libX11
+        pkgs.libICE
+        pkgs.libSM
+        pkgs.libXext
+        pkgs.libXrender
+        pkgs.libXcursor
+        pkgs.libXfixes
+        pkgs.libXi
+        pkgs.libXrandr
+      ]
+    }:$LD_LIBRARY_PATH"
+    exec zsh
+  '';
 }
